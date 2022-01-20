fa801f964a803e744f64c933072084d03f6fbdac
- <->
2022-01-20 16:21:47 +0100

    automated formating

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-
 pub fn run(config: Config) {
     println!("{:#?}", config);
 }
 
-
-
 #[derive(Debug)]
 pub struct Config {
-    pub secret: String,  // The secret which will be split
-    pub shares: u64,  // Number of pieces the secret will be split into
-    pub threshold: u64,  // number of pieces needed to reconstruct the secret
+    pub secret: String, // The secret which will be split
+    pub shares: u64,    // Number of pieces the secret will be split into
+    pub threshold: u64, // number of pieces needed to reconstruct the secret
 }
 
 impl Config {
@@ -20,8 +17,13 @@ impl Config {
         }
 
         let secret = args[1].clone();
-        let shares = args[2].clone().parse::<u64>().expect("Could not parse the second argument (wrong type).");
-        let threshold = args[3].parse::<u64>().expect("Could not parse the third argument (wrong type).");
+        let shares = args[2]
+            .clone()
+            .parse::<u64>()
+            .expect("Could not parse the second argument (wrong type).");
+        let threshold = args[3]
+            .parse::<u64>()
+            .expect("Could not parse the third argument (wrong type).");
 
         Ok(Config {
             secret,
